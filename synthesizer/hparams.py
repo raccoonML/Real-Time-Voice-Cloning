e7aa707c976db0cebbdf2cc68f140edcdd31e7f4
raccoonML <95999607+raccoonML@users.noreply.github.com>
2021-12-21 05:06:22 -0800

    Spanish support.

--- synthesizer/hparams.py
+++ synthesizer/hparams.py
@@ -33,16 +33,16 @@ hparams = HParams(
         preemphasize = True,
 
         ### Tacotron Text-to-Speech (TTS)
-        tts_embed_dims = 512,                       # Embedding dimension for the graphemes/phoneme inputs
+        tts_embed_dims = 256,                       # Embedding dimension for the graphemes/phoneme inputs
         tts_encoder_dims = 256,
         tts_decoder_dims = 128,
-        tts_postnet_dims = 512,
+        tts_postnet_dims = 256,
         tts_encoder_K = 5,
-        tts_lstm_dims = 1024,
+        tts_lstm_dims = 512,
         tts_postnet_K = 5,
         tts_num_highways = 4,
         tts_dropout = 0.5,
-        tts_cleaner_names = ["english_cleaners"],
+        tts_cleaner_names = ["transliteration_cleaners"],
         tts_stop_threshold = -3.4,                  # Value below which audio generation ends.
                                                     # For example, for a range of [-4, 4], this
                                                     # will terminate the sequence at the first

dca669a62105969d034f6a08d4f512f0600bccb1
raccoonML <95999607+raccoonML@users.noreply.github.com>
2022-01-19 02:41:33 -0800

    Switch to basic cleaners.

--- synthesizer/hparams.py
+++ synthesizer/hparams.py
@@ -4,7 +4,7 @@ from tensorflow.contrib.training import HParams
 hparams = HParams(
     # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
     # text, you may want to use "basic_cleaners" or "transliteration_cleaners".
-    cleaners="english_cleaners",
+    cleaners="basic_cleaners",
     
     # If you only have 1 GPU or want to use only one GPU, please set num_gpus=0 and specify the 
     # GPU idx on run. example:
